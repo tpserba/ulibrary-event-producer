e3f39694224145152020fca25ea46c40c794c62f
Tomás Praga <tpraga@serbatic.es>
2023-05-18 11:26:11 +0200

    first

---
+++ src/main/java/com/serbatic/ulearnkafka/ulibraryeventsproducer/UlibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.serbatic.ulearnkafka.ulibraryeventsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlibraryEventsProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serbatic/ulearnkafka/ulibraryeventsproducer/UlibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serbatic.ulearnkafka.ulibraryeventsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
